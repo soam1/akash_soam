de40d6e6982fa2c240064b791e4c0704518fa772
soam1 <soamakash9@gmail.com>
2023-07-07 14:20:41 +0530

    just created landing pages

---
+++ lib/web/landing_page_web.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandingPageWeb extends StatefulWidget {
+  const LandingPageWeb({super.key});
+
+  @override
+  State<LandingPageWeb> createState() => _LandingPageWebState();
+}
+
+class _LandingPageWebState extends State<LandingPageWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
