67d95e233f63621218d9b0f84248a6d4ee0a1698
soam1 <soamakash9@gmail.com>
2023-07-09 18:44:44 +0530

    flutter setup for android, ios and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:akash_soam/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:url_strategy/url_strategy.dart';
+//kIsWeb is a variable which return true if an app is running on web
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyBILxnb5QZjC-bhCAt9X83CQZfssiFIiyg",
+            authDomain: "akash-soam.firebaseapp.com",
+            projectId: "akash-soam",
+            storageBucket: "akash-soam.appspot.com",
+            messagingSenderId: "361229382856",
+            appId: "1:361229382856:web:e4c35fc72eefed7351a1fd",
+            measurementId: "G-G3NEL1DNWH"));
+  } else {
+    await Firebase.initializeApp();
+  }
   setPathUrlStrategy();
   runApp(const MyApp());
 }
